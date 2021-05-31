9a78edc2e63ee9e232d54d6562ed9cbd2aa74cd8
Andreas Mautsch <amautsch>
2021-05-31 12:20:53 +0200

    check integrity

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -1,25 +1,23 @@
 package org.goafabric.calleeservice;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class Application {
 
     public static void main(String... args) {
-        Quarkus.run(args);
+        Quarkus.run(MyApp.class, args);
     }
 
-    /*
     public static class MyApp implements QuarkusApplication {
 
         @Override
         public int run(String... args) throws Exception {
-            System.out.println("Do startup logic here");
+            if ((args.length > 0) && ("-check-integrity".equals(args[0]))) { Quarkus.asyncExit(); }
             Quarkus.waitForExit();
             return 0;
         }
     }
-
-     */
 }
