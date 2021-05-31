7f74e467ae6df57cedde4625c4ab14ef2cb4ed5b
Andreas Mautsch <amautsch>
2021-05-31 12:46:01 +0200

    name fixe

--- src/test/java/org/goafabric/calleeservice/service/NativeCalleeServiceIT.java
+++ src/test/java/org/goafabric/calleeservice/service/NativeCalleeServiceIT.java
@@ -3,7 +3,7 @@ package org.goafabric.calleeservice.service;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeCalleServiceIT extends CalleeServiceTest {
+public class NativeCalleeServiceIT extends CalleeServiceTest {
 
     // Execute the same tests but in native mode.
 }
