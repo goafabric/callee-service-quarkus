80b47b9522b53feb157fd5982f09de681b7ebd8e
Andreas Mautsch <amautsch>
2021-06-04 11:48:55 +0200

    fixes

--- src/test/java/org/goafabric/calleeservice/service/NativeCalleeServiceIT.java
+++ src/test/java/org/goafabric/calleeservice/service/NativeCalleeServiceIT.java
@@ -1,8 +1,8 @@
 package org.goafabric.calleeservice.service;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.NativeImageTest;
 
-@QuarkusIntegrationTest
+@NativeImageTest
 public class NativeCalleeServiceIT extends CalleeServiceTest {
 
     // Execute the same tests but in native mode.
