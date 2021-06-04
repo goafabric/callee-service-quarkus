61a20057280c4b0d7081731b01a6c263da86c8d4
Andreas Mautsch <amautsch>
2021-06-04 13:45:25 +0200

    logging added

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -2,12 +2,15 @@ package org.goafabric.calleeservice;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import lombok.extern.slf4j.Slf4j;
 
 @QuarkusMain
+@Slf4j
 public class Application {
 
     public static void main(String... args) {
         Quarkus.run(args);
+        log.info("Hello from Quarkus");
     }
 
 }
