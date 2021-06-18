3db75edf0c8c1d0860713c7e279bf5491f5c6167
Andreas Mautsch <amautsch>
2021-06-18 08:04:58 +0200

    security added

--- src/main/java/org/goafabric/calleeservice/service/CalleeService.java
+++ src/main/java/org/goafabric/calleeservice/service/CalleeService.java
@@ -12,7 +12,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/callees")
-@RolesAllowed("admin")
+@RolesAllowed("standard_role")
 public class CalleeService {
     @Inject
     CalleeLogic calleeLogic;
