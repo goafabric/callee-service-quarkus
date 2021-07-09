c8c791387cf3467d4edd8b93b0038bba8ab60199
Andreas Mautsch <amautsch>
2021-07-09 12:01:32 +0200

    Long replaced with AtomicLong

--- src/main/java/org/goafabric/calleeservice/logic/CalleeLogic.java
+++ src/main/java/org/goafabric/calleeservice/logic/CalleeLogic.java
@@ -5,10 +5,11 @@ import lombok.SneakyThrows;
 import org.goafabric.calleeservice.service.Callee;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
 public class CalleeLogic {
-    private Long sleepTime = 0l;
+    private AtomicLong sleepTime = new AtomicLong(0L);
 
     public Callee sayMyName(@NonNull String name) {
         sleep();
@@ -23,13 +24,13 @@ public class CalleeLogic {
     }
 
     public Callee setSleepTime(@NonNull Long sleepTime) {
-        this.sleepTime = sleepTime; //you should never change instance variables for a productive app
+        this.sleepTime.set(sleepTime); //you should never change instance variables for a productive app
         return Callee.builder()
                 .message("set sleepTime to: " + sleepTime).build();
     }
 
     @SneakyThrows
     private void sleep() {
-        Thread.sleep(sleepTime);
+        Thread.sleep(sleepTime.get());
     }
 }
