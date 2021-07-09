bd77d9c240411b5135c009077cd8baffb623d4e1
Andreas Mautsch <amautsch>
2021-07-09 14:38:14 +0200

    kotlin branch

--- src/main/java/org/goafabric/calleeservice/crossfunctional/SecurityConfiguration.kt
+++ src/main/java/org/goafabric/calleeservice/crossfunctional/SecurityConfiguration.kt
@@ -1,19 +1,21 @@
-package org.goafabric.calleeservice.crossfunctional;
+package org.goafabric.calleeservice.crossfunctional
 
-import io.quarkus.security.spi.runtime.AuthorizationController;
-import lombok.Getter;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import io.quarkus.security.spi.runtime.AuthorizationController
+import lombok.Getter
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import javax.annotation.Priority
+import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Alternative
+import javax.interceptor.Interceptor
 
-import javax.annotation.Priority;
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Alternative;
-import javax.interceptor.Interceptor;
-
-@Getter
 @Alternative
 @Priority(Interceptor.Priority.LIBRARY_AFTER)
 @ApplicationScoped
-public class SecurityConfiguration extends AuthorizationController {
+class SecurityConfiguration(
     @ConfigProperty(name = "security.authentication.enabled", defaultValue = "true")
-    boolean isAuthorizationEnabled;
+    val authorizationEnabled : Boolean) : AuthorizationController() {
+
+    override fun isAuthorizationEnabled(): Boolean {
+        return this.authorizationEnabled
+    }
 }
