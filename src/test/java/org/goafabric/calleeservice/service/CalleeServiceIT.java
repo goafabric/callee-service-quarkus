51bd737120fc41770c0c7e3901352a7ca189d179
Andreas Mautsch <amautsch>
2021-07-15 14:44:36 +0200

    integrationtest added

--- src/test/java/org/goafabric/calleeservice/service/CalleeServiceIT.java
+++ src/test/java/org/goafabric/calleeservice/service/CalleeServiceIT.java
@@ -1,29 +1,7 @@
 package org.goafabric.calleeservice.service;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.junit.jupiter.api.Test;
-
-import javax.inject.Inject;
 
 @QuarkusIntegrationTest
-public class CalleeServiceIT {
-    @Inject
-    @RestClient
-    CalleeServiceAdapter calleeServiceAdapter;
-
-    @Test
-    public void sayMyName() {
-        calleeServiceAdapter.sayMyName("Heisenberg");
-    }
-
-    @Test
-    public void sayMyOtherName() {
-        calleeServiceAdapter.sayMyOtherName("Heisenberg");
-    }
-
-    @Test
-    public void setSleepTime() {
-        calleeServiceAdapter.setSleepTime(0L);
-    }
+public class CalleeServiceIT extends CalleeServiceTest{
 }
