67bee3beb689291e28c0775a77a3a63799cd4f36
Andreas Mautsch <amautsch>
2021-07-16 08:18:25 +0200

    integration tests added

--- src/test/java/org/goafabric/calleeservice/service/CalleeServiceTest.java
+++ src/test/java/org/goafabric/calleeservice/service/CalleeServiceTest.java
@@ -3,6 +3,8 @@ package org.goafabric.calleeservice.service;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Base64;
+
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
@@ -11,14 +13,16 @@ class CalleeServiceTest {
     @Test
     public void sayMyName() {
         given()
-          .when().get("/callees/sayMyName?name=Heisenberg")
-          .then()
-             .statusCode(200);
+                .auth().basic(new String(Base64.getDecoder().decode("YWRtaW4=")), new String(Base64.getDecoder().decode("YWRtaW4=")))
+                .when().get("/callees/sayMyName?name=Heisenberg")
+                .then()
+                .statusCode(200);
     }
 
     @Test
     public void sayMyOtherName() {
         given()
+                .auth().basic(new String(Base64.getDecoder().decode("YWRtaW4=")), new String(Base64.getDecoder().decode("YWRtaW4=")))
                 .when().get("/callees/sayMyOtherName/Andreas")
                 .then()
                     .statusCode(200);
@@ -27,6 +31,7 @@ class CalleeServiceTest {
     @Test
     public void setSleepTime() {
         given()
+                .auth().basic(new String(Base64.getDecoder().decode("YWRtaW4=")), new String(Base64.getDecoder().decode("YWRtaW4=")))
                 .when().get("/callees/setSleepTime?sleepTime=0")
                 .then()
                 .statusCode(200);
