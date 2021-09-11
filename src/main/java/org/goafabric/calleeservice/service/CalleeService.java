d518a5acf927f472e20788c611b809d8eccd6080
Andreas Mautsch <amautsch>
2021-09-11 09:40:17 +0200

    keycloak changes

--- src/main/java/org/goafabric/calleeservice/service/CalleeService.java
+++ src/main/java/org/goafabric/calleeservice/service/CalleeService.java
@@ -1,23 +1,22 @@
 package org.goafabric.calleeservice.service;
 
-import io.quarkus.security.identity.SecurityIdentity;
 import org.goafabric.calleeservice.logic.CalleeLogic;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/callees")
 @RolesAllowed("standard_role")
-//@Authenticated
 @Produces(MediaType.APPLICATION_JSON)
 public class CalleeService {
     @Inject
     CalleeLogic calleeLogic;
-
-    @Inject
-    SecurityIdentity securityIdentity;
 
     @GET
     @Path("sayMyName")
