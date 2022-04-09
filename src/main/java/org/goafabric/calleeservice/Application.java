7ba2c95001ac6d1c7dae0b8a676dab1d1b4afd00
Andreas Mautsch <amautsch>
2022-04-09 13:03:48 +0200

    easier security

--- src/main/java/org/goafabric/calleeservice/Application.java
+++ src/main/java/org/goafabric/calleeservice/Application.java
@@ -2,7 +2,15 @@ package org.goafabric.calleeservice;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.security.spi.runtime.AuthorizationController;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import javax.interceptor.Interceptor;
 
 @QuarkusMain
 @Slf4j
@@ -12,4 +20,11 @@ public class Application {
         Quarkus.run(args);
     }
 
+    @Getter
+    @Alternative
+    @ApplicationScoped @Priority(Interceptor.Priority.LIBRARY_AFTER)
+    static class SecurityConfiguration extends AuthorizationController {
+        @ConfigProperty(name = "security.authentication.enabled", defaultValue = "true")
+        boolean isAuthorizationEnabled;
+    }
 }
