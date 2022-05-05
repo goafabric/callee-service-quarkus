8af4a68471f1008be1148cf71ea5dd1100de47dc
Andreas Mautsch <andreas>
2022-05-05 14:40:22 +0200

    oidc updated

--- src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
+++ src/main/java/org/goafabric/calleeservice/crossfunctional/HttpInterceptor.java
@@ -1,8 +1,16 @@
 package org.goafabric.calleeservice.crossfunctional;
 
+import io.quarkus.oidc.OidcRequestContext;
+import io.quarkus.oidc.OidcTenantConfig;
+import io.quarkus.oidc.TenantConfigResolver;
 import io.quarkus.security.identity.SecurityIdentity;
+import io.smallrye.mutiny.Uni;
+import io.vertx.ext.web.RoutingContext;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -13,7 +21,9 @@ import java.io.IOException;
 
 @Slf4j
 @Provider
-public class HttpInterceptor implements ContainerRequestFilter, ContainerResponseFilter {
+
+@ApplicationScoped
+public class HttpInterceptor implements ContainerRequestFilter, ContainerResponseFilter, TenantConfigResolver {
     @Inject SecurityIdentity securityIdentity;
     private static final ThreadLocal<String> tenantId = new ThreadLocal<>();
     private static final ThreadLocal<String> userName = new ThreadLocal<>();
@@ -32,6 +42,27 @@ public class HttpInterceptor implements ContainerRequestFilter, ContainerRespons
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
         tenantId.remove();
         userName.remove();
+    }
+
+    public Uni<OidcTenantConfig> resolve(RoutingContext routingContext, OidcRequestContext<OidcTenantConfig> requestContext) {
+        return createOidcConfig(routingContext.request().getHeader("X-TenantId"));
+    }
+
+    private Uni<OidcTenantConfig> createOidcConfig(String tenantId) {
+        final OidcTenantConfig tenantConfig = new OidcTenantConfig();
+        final Config config = ConfigProvider.getConfig();
+
+        tenantConfig.setTenantId(tenantId);
+        tenantConfig.setApplicationType(
+                OidcTenantConfig.ApplicationType.valueOf(config.getValue("quarkus.oidc.application-type", String.class).toUpperCase()));
+
+        final OidcTenantConfig.Roles roles = new OidcTenantConfig.Roles();
+        roles.setSource(OidcTenantConfig.Roles.Source.accesstoken); //config.getValue("quarkus.oidc.roles.source", OidcTenantConfig.Roles.Source.class));
+        tenantConfig.setRoles(roles);
+
+        tenantConfig.setClientId(config.getValue("quarkus.oidc.client-id", String.class));
+        tenantConfig.setAuthServerUrl(config.getValue("quarkus.oidc.auth-server-url", String.class) + tenantId);
+        return Uni.createFrom().item(tenantConfig);
     }
 
 }
