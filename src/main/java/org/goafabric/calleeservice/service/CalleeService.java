6925fae8f849f1e75d04eacda52744593e21d712
Andreas Mautsch <amautsch>
2023-03-03 12:45:04 +0100

    refactoring

--- src/main/java/org/goafabric/calleeservice/service/CalleeService.java
+++ src/main/java/org/goafabric/calleeservice/service/CalleeService.java
@@ -3,7 +3,6 @@ package org.goafabric.calleeservice.service;
 import org.goafabric.calleeservice.logic.CalleeLogic;
 
 import javax.annotation.security.RolesAllowed;
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -11,8 +10,11 @@ import javax.ws.rs.core.MediaType;
 @RolesAllowed("standard_role")
 @Produces(MediaType.APPLICATION_JSON)
 public class CalleeService {
-    @Inject
-    CalleeLogic calleeLogic;
+    private final CalleeLogic calleeLogic;
+
+    public CalleeService(CalleeLogic calleeLogic) {
+        this.calleeLogic = calleeLogic;
+    }
 
     @GET
     @Path("sayMyName")
