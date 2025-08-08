41f2a83f525c678383c5b99e457d24501cc606f9
Andreas Mautsch <amautsch@gmx.de>
2025-08-08 02:01:37 +0000

    Update dependency org.assertj:assertj-core to v3.27.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
 	constraints {
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-		testImplementation("org.assertj:assertj-core:3.27.3")
+		testImplementation("org.assertj:assertj-core:3.27.4")
 	}
 
 	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.25.1"))
