1d5ac1fd705a3f157f3113910fe8722b8741711f
Andreas Mautsch <amautsch@gmx.de>
2026-01-28 13:07:35 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val dockerRegistry = "goafabric"
 plugins {
 	java
 	jacoco
-	id("io.quarkus") version "3.30.6"
+	id("io.quarkus") version "3.31.1"
 	id("net.researchgate.release") version "3.1.0"
 }
 
@@ -19,10 +19,10 @@ dependencies {
 	constraints {
 		implementation("org.mapstruct:mapstruct:1.6.3")
 		annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-		testImplementation("org.assertj:assertj-core:3.27.6")
+		testImplementation("org.assertj:assertj-core:3.27.7")
 	}
 
-	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.30.6"))
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.31.1"))
 }
 dependencies {
 	//web
