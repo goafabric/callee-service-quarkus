39b0b0a29b557be259748ac42d5bee59d446ba95
Andreas Mautsch <amautsch>
2026-03-08 10:19:55 +0100

    downsized

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,8 +43,7 @@ dependencies {
 	implementation("io.quarkus:quarkus-container-image-jib")
 
 	//kotlin
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.quarkus:quarkus-kotlin")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	//test
