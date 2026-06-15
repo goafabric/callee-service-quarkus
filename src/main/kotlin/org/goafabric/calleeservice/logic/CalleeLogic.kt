50dbe7a68a996107ea12d22b71a610851fbf0917
Andreas Mautsch <amautsch>
2026-06-15 08:38:11 +0200

    chang

--- src/main/kotlin/org/goafabric/calleeservice/logic/CalleeLogic.kt
+++ src/main/kotlin/org/goafabric/calleeservice/logic/CalleeLogic.kt
@@ -5,15 +5,12 @@ import org.goafabric.calleeservice.controller.dto.Callee
 
 @ApplicationScoped
 class CalleeLogic {
-    fun sayMyName(name: String?): Callee {
-        return Callee("0", "Your name is: $name")
-    }
+    fun sayMyName(name: String)
+        = Callee("0", "Your name is: $name")
 
-    fun sayMyOtherName(name: String?): Callee {
-        return Callee("0", "Your other name is: $name")
-    }
+    fun sayMyOtherName(name: String)
+        = Callee("0", "Your other name is: $name")
 
-    fun save(callee: Callee): Callee {
-        return Callee("0", "Storing your message: " + callee.message)
-    }
+    fun save(callee: Callee)
+        = Callee("0", "Storing your message: " + callee.message)
 }
