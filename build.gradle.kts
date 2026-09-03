76cf4f6ecebcdb750753426970b5c4b93f18e480
Andreas Mautsch <amautsch@gmx.de>
2026-09-03 04:02:59 +0000

    Update dependency io.quarkiverse.mcp:quarkus-mcp-server-http to v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 	}
 
 	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.39.2"))
-	implementation("io.quarkiverse.mcp:quarkus-mcp-server-http:1.13.2") // //2.0.0.CR1
+	implementation("io.quarkiverse.mcp:quarkus-mcp-server-http:2.0.0") // //2.0.0.CR1
 }
 
 dependencies {
