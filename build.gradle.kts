314b79c87a6975a9f3c240408ec5d19512ea911d
Andreas Mautsch <amautsch@gmx.de>
2026-09-25 17:00:20 +0000

    Update dependency com.tngtech.archunit:archunit-junit5 to v1.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 dependencies {
 	constraints {
 		testImplementation("org.assertj:assertj-core:3.27.7")
-		testImplementation("com.tngtech.archunit:archunit-junit5:1.5.0")
+		testImplementation("com.tngtech.archunit:archunit-junit5:1.5.1")
 	}
 
 	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.40.0"))
